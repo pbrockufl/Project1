d57bcec1fc82f1aff198333aabace49908863c2a
pbrockufl <pbrock@ufl.edu>
2025-02-20 20:06:15 -0500

    Update riemann.py

--- riemann.py
+++ riemann.py
@@ -11,3 +11,12 @@ def trapezoid(x_values: np.ndaraay, func: np.ufunc):
     heights = (func(x_values[:-1]) + func(x_values[1:])) / 2
     areas = widths * heights
     return np.sum(areas)
+
+def simpson(x_values: np.ndarray, func: np.ufunc):
+    Sum = 0
+    for i in range(0, len(x_values -2, 2)): #Step size 2
+        a, b, c = x_values[i], x_values[i+1], x_values[i+2]
+        h = (c-a) / 2 #Half interval
+
+        Sum += (c-a) / 6 * (func(a) + 4 * func(b) + 4 * func(c))
+    return Sum
