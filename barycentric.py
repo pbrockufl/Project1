5364d93da074b0d1613b07e7d84a3f4281d4b4e3
razo-alexander <149010490+razo-alexander@users.noreply.github.com>
2025-02-20 19:59:43 -0500

    hi

--- barycentric.py
+++ barycentric.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> np.ndarray:
+def get_bar_coordinates(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> np.ndarray:
     x1, x2, x3 = triangle_coordinates[0]
     y1, y2, y3 = triangle_coordinates[1]
     x, y = point_coordinates
@@ -13,12 +13,10 @@ def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordina
 
     return np.array([lamda1, lamda2, lamda3])
 
-def get_cartesian_coordinates(triangle_coordinates: np.ndarray, barycentric_coordinates: np.ndarray) -> np.ndarray:
-    return np.dot(triangle_coordinates, barycentric_coordinates)
-
-import numpy as np
+def get_cartesian_coordinates(triangle_coordinates: np.ndarray, bar_coordinates: np.ndarray) -> np.ndarray:
+    return np.dot(triangle_coordinates, bar_coordinates)
 
 def is_inside_triangle(triangle_coordinates, point_coordinates):
-  bar_coordinates = get_barycentric_coordinates(triangle_coordinates, point_coordinates)
+  bar_coordinates = get_bar_coordinates(triangle_coordinates, point_coordinates)
   return np.all(bar_coordinates >= 0)
 
