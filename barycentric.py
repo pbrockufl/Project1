0aaf68b5f229b55cab090bba19e0f2032fcdb6a4
razo-alexander <149010490+razo-alexander@users.noreply.github.com>
2025-02-21 12:21:38 -0500

    hi

--- barycentric.py
+++ barycentric.py
@@ -7,8 +7,11 @@ def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordina
 
     denom = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
 
-    lambda1 = ((x2 - x) * (y3 - y) - (x3 - x) * (y2 - y)) / denom
-    lambda2 = ((x3 - x) * (y1 - y) - (x1 - x) * (y3 - y)) / denom
+    if abs(denom) < 1e-10:
+        return np.array([-1, -1, -1])
+
+    lambda1 = ((x - x2) * (y3 - y2) - (x3 - x2) * (y - y2)) / denom
+    lambda2 = ((x - x3) * (y1 - y3) - (x1 - x3) * (y - y3)) / denom
     lambda3 = 1 - lambda1 - lambda2
 
     return np.array([lambda1, lambda2, lambda3])
@@ -16,7 +19,8 @@ def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordina
 def get_cartesian_coordinates(triangle_coordinates: np.ndarray, bar_coordinates: np.ndarray) -> np.ndarray:
     return np.dot(triangle_coordinates, bar_coordinates)
 
-def is_inside_triangle(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) ->np.ndarray:
+def is_inside_triangle(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> bool:
   bar_coordinates = get_barycentric_coordinates(triangle_coordinates, point_coordinates)
+  if np.any(bar_coordinates < -0.01):
+      return False
   return np.all(bar_coordinates >= 0)
-
