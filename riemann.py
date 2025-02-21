ddea5866d608bac824f5f9e3b2eac6d279fb386c
Santi230705 <santiago.gomez1@ufl.edu>
2025-02-20 19:59:51 -0500

    Update riemann.py

--- riemann.py
+++ riemann.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def left_endpoint(x_values, func):
+def left_endpoint(x_values: np.ndarray, func: np.ufunc):
     widths = np.diff(x_values)
     heights = func(x_values[:-1])
     areas = widths * heights
     return np.sum(areas)
 
-def trapezoid(x_values, func):
+def trapezoid(x_values: np.ndaraay, func: np.ufunc):
     widths = np.diff(x_values)
     heights = (func(x_values[:-1]) + func(x_values[1:])) / 2
     areas = widths * heights
