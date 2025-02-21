048bbac93a64d44f1a37139bc6adb8a9c89feec1
razo-alexander <149010490+razo-alexander@users.noreply.github.com>
2025-02-20 20:04:57 -0500

    hi

--- barycentric.py
+++ barycentric.py
@@ -7,11 +7,11 @@ def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordina
 
     denom = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
 
-    lamda1 = ((x2 - x) * (y3 - y) - (x3 - x) * (y2 - y)) / denom
-    lamda2 = ((x3 - x) * (y1 - y) - (x1 - x) * (y3 - y)) / denom
-    lamda3 = 1 - lamda1 - lamda2
+    lambda1 = ((x2 - x) * (y3 - y) - (x3 - x) * (y2 - y)) / denom
+    lambda2 = ((x3 - x) * (y1 - y) - (x1 - x) * (y3 - y)) / denom
+    lambda3 = 1 - lambda1 - lambda2
 
-    return np.array([lamda1, lamda2, lamda3])
+    return np.array([lambda1, lambda2, lambda3])
 
 def get_cartesian_coordinates(triangle_coordinates: np.ndarray, bar_coordinates: np.ndarray) -> np.ndarray:
     return np.dot(triangle_coordinates, bar_coordinates)
