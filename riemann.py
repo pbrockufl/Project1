5433b4fceb7f1a5ca038c64204ca5f3f4aeb2b08
parkerbrock <pbrock@ufl.edu>
2025-02-26 19:00:39 -0500

    done

--- riemann.py
+++ riemann.py
@@ -13,10 +13,11 @@ def trapezoid(x_values: np.ndarray, func: np.ufunc):
     return np.sum(areas)
 
 def simpson(x_values: np.ndarray, func: np.ufunc):
-    n = len(x_values) #Subintervals
+    #(b-a)/2 (f(a) + 4((a-b)/2) + f(b))
+    n = len(x_values) # Calculates intervals
     Sum = 0
     for i in range(0, n-2, 2): #Step size 2
-        a, b, c = x_values[i], x_values[i+1], x_values[i+2]
+        a, b, c = x_values[i], x_values[i+1], x_values[i+2] #Values within interval
 
-        Sum += (c-a) / 6 * (func(a) + 4 * func(b) + func(c))
+        Sum += (c-a) / 6 * (func(a) + 4 * func(b) + func(c)) #Simpsons function
     return Sum
